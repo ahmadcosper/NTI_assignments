77760ea3c41d2ee47488c3d957bbb1acccc83251
احمد شاكر توفيق محمد كسبر stu <325250246@sha.edu.eg>
2026-07-13 20:21:26 +0300

    submit task1 day2

--- day_2/tasks/d2_task.js
+++ day_2/tasks/d2_task.js
@@ -1,20 +1,22 @@
 var username = document.getElementById("username");
 var password = document.getElementById("password");
-function login() { 
-    if (username.value === "admin" && password.value === "admin1234") 
-    {
-        console.log("Login successful" + "your username is: " + username.value);
-    }
+function register() { 
     
-    else if (password.value.length < 8)
-    {
-        alert("password must be at least 8 characters long");
-    }
-    else  (username.value.isEmpty() || password.value.isEmpty())
+    if (username.value === "" || password.value === "")
     {
         alert("username & password are required");
     }
-    
+    else if (password.value.length < 8)
+    {
+        alert("password must be at least 8 characters long");
+     
+    }
+    else 
+    {
+        console.log("Registration successful " + "your username is: " + username.value);
+       
+    }
+   
 }
 function reset() {
     username.value = "";
